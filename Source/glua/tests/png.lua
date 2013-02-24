c3b36e378f2f071d4f1f6015714834f6688ecf1c
Galin Bajlekov <g.bajlekov@gmail.com>
2013-02-24 23:19:22 +0100

    Remove glua

--- Source/glua/tests/png.lua
+++
@@ -1,9 +0,0 @@
-package.path = "../?.lua;../?/init.lua;" .. package.path
-
-local png = require 'glua.lib.png'
-
-local bitmap, width, height, channels = png.Bitmap('textures/GraniteWall-ColorMap.png')
-
-print('width='..width)
-print('height='..height)
-print('channels='..channels)
