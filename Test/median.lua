49ad447589417ac8e0953c0e6bcdead9fb23cbae
Galin Bajlekov <g.bajlekov@gmail.com>
2013-03-13 16:23:27 +0100

    Fast SIMD median implementation using ISPC

--- Test/median.lua
+++ Test/median.lua
@@ -17,24 +17,30 @@
 
 -- fast median filtering with a minimum number of compares, C and lua implementation
 
+-- ISPC implementation:
+os.execute ("ispc --opt=fast-math --pic -o Test/median.o Test/median.ispc") print("ISPC")
+--os.execute ("gcc -O3 -std=gnu99 -ffast-math -march=native -fPIC -c Test/median.c -o Test/median.o") print("vectorised GCC")
+--os.execute ("clang -O3 -std=gnu99 -ffast-math -march=native -fPIC -c Test/median.c -o Test/median.o") print("LLVM")
+os.execute ("gcc -m64 -shared -o Test/libmedian.so Test/median.o")
+
 
 local ffi = require("ffi")
-local C = ffi.load("./libmedian.so")
-ffi.cdef [[	void medianD(double* in, double* out, int xmax, int ymax) ]]
+local C = ffi.load("./Test/libmedian.so")
+ffi.cdef [[	void medianD(float* in, float* out, int xmax, int ymax) ]]
 
 local median
 do
-	local pix = ffi.new("double[9]")
-	local A = ffi.new("short[19]", 1,4,7,0,3,6,1,4,7,0,5,4,3,1,2,4,4,6,4)
-	local B = ffi.new("short[19]", 2,5,8,1,4,7,2,5,8,3,8,7,6,4,5,7,2,4,2)
+	local pix = ffi.new("float[9]")
+	local A = ffi.new("int[19]", 1,4,7,0,3,6,1,4,7,0,5,4,3,1,2,4,4,6,4)
+	local B = ffi.new("int[19]", 2,5,8,1,4,7,2,5,8,3,8,7,6,4,5,7,2,4,2)
 	local function sort(a, b)
 		if pix[a]>pix[b] then
 			pix[a], pix[b] = pix[b], pix[a]
 		end
 	end
 	median = function(i, o, xmax, ymax)
-		for x = 1, xmax-2 do
-			for y = 1, ymax-2 do
+		for y = 1, ymax-2 do
+			for x = 1, xmax-2 do
 				pix[0] = i[(y-1)*xmax+x-1];
 				pix[1] = i[y*xmax+x-1];
 				pix[2] = i[(y+1)*xmax+x-1];
@@ -55,26 +61,30 @@ do
 end
 	
 local size = 4000
+local imgI = ffi.new("float[?]", size*size)
+local imgO1 = ffi.new("float[?]", size*size)
+local imgO2 = ffi.new("float[?]", size*size)
 
-local imgI = ffi.new("double[?]", size*size)
-local imgO = ffi.new("double[?]", size*size)
+for i = 0, size*size-1 do
+	imgI[i] = math.random(4096)
+end
 
+--print("start")
 local t = os.clock()
-C.medianD(imgI, imgO, size, size)
+C.medianD(imgI, imgO2, size, size)
 print("C: ",os.clock()-t)
 local t = os.clock()
-median(imgI, imgO, size, size)
+median(imgI, imgO1, size, size)
 print("Lua: ",os.clock()-t)
 local t = os.clock()
-C.medianD(imgI, imgO, size, size)
+C.medianD(imgI, imgO2, size, size)
 print("C: ",os.clock()-t)
 local t = os.clock()
-median(imgI, imgO, size, size)
-print("Lua: ",os.clock()-t)
-local t = os.clock()
-C.medianD(imgI, imgO, size, size)
-print("C: ",os.clock()-t)
-local t = os.clock()
-median(imgI, imgO, size, size)
+median(imgI, imgO1, size, size)
 print("Lua: ",os.clock()-t)
 
+
+for i = 100*400+200, 100*400+300 do
+	assert(imgO1[i]-imgO2[i]==0)
+	--print(imgO1[i]-imgO2[i])
+end
