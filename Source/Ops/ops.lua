8d4c9f0d1f0bc8934ceda8de242675cb81937e5f
Galin Bajlekov <g.bajlekov@gmail.com>
2013-03-25 11:43:37 +0100

    Port ops to new wrapper code

--- Source/Ops/ops.lua
+++ Source/Ops/ops.lua
@@ -26,186 +26,11 @@ ops.layer = require("opsLayer")
 
 require("mathtools")
 
--- generic pixel function loop
-local startstring_matrix = [[
-							local s = __global.state
-							local b = __global.buf
-							local p = __global.params
-							local progress	= __global.progress
-							local inst	= __global.instance
-							local instmax	= __global.instmax
-							
-							for x = inst, s.xmax-1, instmax do
-								if progress[instmax]==-1 then break end
-								for y = 0, s.ymax-1 do
-									s:up(x, y)
-]]
-
-local endstring_matrix = [[
-									end
-								progress[inst] = x - inst
-							end
-							progress[inst] = -1
-]]
-
-
---for value/colour processing:
---local startstring_single = [[ __pp = 0 ]]
---local endstring_single = [[ progress[__instance+1] = -1 ]]
-
--- TODO: refactor ops, remove inner loops! see Test/opsInterface.lua
-ops.strings = {
-
-	invert = [[ -- 2, 1
-	for c = 0, 2 do
-		b[3]:set( (1-b[1]:get(c))*b[2]:get[2](c) + b[1]:get(c)*(1-b[2]:get(c)), c)
-	end ]],
-
-	mixer = [[ -- 4, 1
-	b[5]:set( b[2]:get(0)*b[1]:get(0) + b[2]:get(1)*b[1]:get(1) + b[2]:get(2)*b[1]:get(2), 0)
-	b[5]:set( b[3]:get(0)*b[1]:get(0) + b[3]:get(1)*b[1]:get(1) + b[3]:get(2)*b[1]:get(2), 1)
-	b[5]:set( b[4]:get(0)*b[1]:get(0) + b[4]:get(1)*b[1]:get(1) + b[4]:get(2)*b[1]:get(2), 2)
-	]],
-
-	cstransform = [[ --1, 1, {9}
-	local c1, c2, c3 = b[1]:get3()
-	local p1, p2, p3
-	p1 = p[1]*c1 + p[2]*c2 + p[3]*c3
-	p2 = p[4]*c1 + p[5]*c2 + p[6]*c3
-	p3 = p[7]*c1 + p[8]*c2 + p[9]*c3
-	b[2]:set3(p1, p2, p3)
-	]],
-
-	copy = [[ -- 1, 1
-	b[2]:set3( b[1]:get3())
-	]],
-
-	hsxedit = [[ -- 2,1
-	local x = b[2]:get(0)+b[1]:get(0)
-	x = x>1 and x-1 or x
-	b[3]:set( x, 0)
-	b[3]:set( b[2]:get(1)*b[1]:get(1), 1)
-	b[3]:set( b[2]:get(2)*b[1]:get(2), 2)
-	]],
-
-	lchedit = [[ -- 2,1
-	local x = b[2]:get(2)+b[1]:get(2)
-	x = x>1 and x-1 or x
-	b[3]:set( b[2]:get(0)*b[1]:get(0), 0)
-	b[3]:set( b[2]:get(1)*b[1]:get(1), 1)
-	b[3]:set( x, 2)
-	]],
-
-	rgbedit = [[ -- 3,1
-	for c = 0, 2 do
-		b[4]:set( b[1]:get(c)*b[2]:get(c)+b[3]:get(c) , c)
-	end	]],
-
-	compose = [[ -- 3,1
-	b[4]:set3( b[1]:get(0), b[2]:get(1), b[3]:get(2) )
-	]],
-
-	decompose = [[ -- 1,3
-		b[2]:set(b[1]:get(0))
-		b[3]:set(b[1]:get(1))
-		b[4]:set(b[1]:get(2))
-	]],
-
-	merge = [[ -- 3,1
-	for c = 0, 2 do
-		b[4]:set( b[1]:get(c)*b[3]:get(c) + b[2]:get(c)*(1-b[3]:get(c)), c)
-	end	]],
-
-	add = [[ -- 2,1
-	for c = 0, 2 do
-		b[3]:set( b[1]:get(c) + b[2]:get(c), c)
-	end	]],
-
-	sub = [[ -- 2,1
-	for c = 0, 2 do
-		b[3]:set( b[1]:get(c) - b[2]:get(c), c)
-	end	]],
-
-	mult = [[ -- 2,1
-	for c = 0, 2 do
-		b[3]:set( b[1]:get(c) * b[2]:get(c), c)
-	end	]],
-
-	div = [[ -- 2,1
-	for c = 0, 2 do
-		b[3]:set( b[1]:get(c) / b[2]:get(c), c)
-	end	]],
-
-	compMult = [[ -- 4, 2
-	for c = 0, 2 do
-		b[5]:set( b[1]:get(c)*b[3]:get(c) - b[2]:get(c)*b[4]:get(c), c)
-		b[6]:set( b[1]:get(c)*b[4]:get(c) + b[2]:get(c)*b[3]:get(c), c)
-	end
-	]],
-
-	zero = [[ -- 0,1
-	for c = 0, 2 do
-		b[1]:set( 0, c)
-	end	]],
-
-	equaliseGB = [[ -- 1,1
-		local GB = (b[1]:get(1)+b[1]:get(2))/2
-		b[2]:set( GB, 1)
-		b[2]:set( GB, 2)
-	]],
-
-	invertR_GB = [[ -- 1,1
-		local GB = 1-b[1]:get(0)
-		b[2]:set( GB, 1)
-		b[2]:set( GB, 2)
-	]],
-}
-
---[[
-do
-	local function filter(func, flag)
-		for x = __instance, xmax/2, __tmax do
-			if progress[0]==-1 then break end
-			for y = 0, ymax/2 do
-				local gauss
-				local size = 128 --math.sqrt(xmax^2+ymax^2)
-				gauss = func(math.sqrt(x^2+y^2), get[1](0)*size)
-				gauss = gauss + (flag and func(math.sqrt((xmax-x+1)^2+y^2), get[1](0)*size) or 0)
-				gauss = gauss + (flag and func(math.sqrt(x^2+(ymax-y+1)^2), get[1](0)*size) or 0)
-				gauss = gauss + (flag and func(math.sqrt((xmax-x+1)^2+(ymax-y+1)^2), get[1](0)*size) or 0)
-				gauss = gauss * get[2]() + 1 - get[2]()
-				set3xy[1](gauss, gauss, gauss,x,y)
-				if x~=0 then set3xy[1](gauss, gauss, gauss, xmax-x,y) end
-				if y~=0 then set3xy[1](gauss, gauss, gauss, x, ymax-y) end
-				if x~=0 and y~=0 then set3xy[1](gauss, gauss, gauss, xmax-x, ymax-y) end
-			end
-			progress[__instance+1] = x - __instance
-		end
-		progress[__instance+1] = -1
-	end
-
-	function ops.gauss() return filter(math.func.gauss) end
-	function ops.lorenz() return filter(math.func.lorenz) end
-	function ops.gauss_wrap() return filter(math.func.gauss, true) end
-	function ops.lorenz_wrap() return filter(math.func.lorenz, true) end
-end
---]]
-
--- construct all pixel functions from ops.strings
-for k, v in pairs(ops.strings) do
-	ops[k] = loadstring(startstring_matrix..v..endstring_matrix)
-end
-ops.strings = nil
-
-ops.empty = function() __global.progress[__global.inst] = -1 end
-
-
 
 -- refactor to avoid small loops along Z-dim
 -- and to avoid compiling of text functions
 -- and to avoid overhead for single channel ops
 
-do
 -- wrap single channel ops in multichannel functions:
 local function wrapChan(fun)
 	return function(b, p, zSize)
@@ -263,22 +88,154 @@ local function wrapLoop(fun, preFun)
 	end
 end
 
--- test code:
---[[
---write function(b, p, z):
-local function copy(b, p, z)
+-- make wrapper functions available through __global.tools
+__global.tools = {wrapChan=wrapChan, wrapLoop=wrapLoop}
+
+local function invert(b, p, c) -- 2, 1
+	b[3]:set( (1-b[1]:get(c))*b[2]:get(c) + b[1]:get(c)*(1-b[2]:get(c)), c)
+end
+ops.invert = wrapLoop(wrapChan(invert))
+
+
+local function mixer(b, p) -- 4, 1
+	b[5]:set( b[2]:get(0)*b[1]:get(0) + b[2]:get(1)*b[1]:get(1) + b[2]:get(2)*b[1]:get(2), 0)
+	b[5]:set( b[3]:get(0)*b[1]:get(0) + b[3]:get(1)*b[1]:get(1) + b[3]:get(2)*b[1]:get(2), 1)
+	b[5]:set( b[4]:get(0)*b[1]:get(0) + b[4]:get(1)*b[1]:get(1) + b[4]:get(2)*b[1]:get(2), 2)
+end
+ops.mixer = wrapLoop(mixer)
+
+local function cstransform(b, p) -- 1, 1, 9
+	local c1, c2, c3 = b[1]:get3()
+	local p1, p2, p3
+	p1 = p[1]*c1 + p[2]*c2 + p[3]*c3
+	p2 = p[4]*c1 + p[5]*c2 + p[6]*c3
+	p3 = p[7]*c1 + p[8]*c2 + p[9]*c3
+	b[2]:set3(p1, p2, p3)
+end 
+ops.cstransform = wrapLoop(cstransform)
+
+local function copy(b, p, z) -- 1, 1
 	b[2]:set(b[1]:get(z), z)
 end
 ops.copy = wrapLoop(wrapChan(copy))
 
-local function copy(b, p)
-	b[2]:set3(b[1]:get3())
+
+local function hsxedit(b, p) -- 2, 1
+	local x = b[2]:get(0)+b[1]:get(0)
+	x = x>1 and x-1 or x
+	b[3]:set( x, 0)
+	b[3]:set( b[2]:get(1)*b[1]:get(1), 1)
+	b[3]:set( b[2]:get(2)*b[1]:get(2), 2)
 end
-ops.copy = wrapLoop(copy)
+ops.hsxedit = wrapLoop(hsxedit)
+
+local function lchedit(b, p) -- 2, 1
+local x = b[2]:get(2)+b[1]:get(2)
+x = x>1 and x-1 or x
+b[3]:set( b[2]:get(0)*b[1]:get(0), 0)
+b[3]:set( b[2]:get(1)*b[1]:get(1), 1)
+b[3]:set( x, 2)
+end
+ops.lchedit = wrapLoop(lchedit)
+
+local function rgbedit(b, p, c) -- 3, 1
+	b[4]:set( b[1]:get(c)*b[2]:get(c)+b[3]:get(c) , c)
+end
+ops.rgbedit = wrapLoop(wrapChan(rgbedit))
+
+local function compose(b, p) -- 3,1
+b[4]:set3( b[1]:get(0), b[2]:get(1), b[3]:get(2) )
+end
+ops.compose = wrapLoop(compose)
+
+local function decompose(b, p) -- 1,3
+	b[2]:set(b[1]:get(0))
+	b[3]:set(b[1]:get(1))
+	b[4]:set(b[1]:get(2))
+end
+ops.decompose = wrapLoop(decompose)
+
+local function merge(b, p, c) -- 3,1
+	b[4]:set( b[1]:get(c)*b[3]:get(c) + b[2]:get(c)*(1-b[3]:get(c)), c)
+end
+ops.merge = wrapLoop(wrapChan(merge))
+
+--ops.strings = {
+
+local function add(b, p, c) -- 2, 1
+	b[3]:set( b[1]:get(c) + b[2]:get(c), c)
+end
+ops.add = wrapLoop(wrapChan(add))
+
+local function sub(b, p, c) -- 2, 1
+	b[3]:set( b[1]:get(c) - b[2]:get(c), c)
+end
+ops.sub = wrapLoop(wrapChan(sub))
+
+local function mul(b, p, c) -- 2, 1
+	b[3]:set( b[1]:get(c) * b[2]:get(c), c)
+end
+ops.mul = wrapLoop(wrapChan(mul))
+
+local function div(b, p, c) -- 2, 1
+	b[3]:set( b[1]:get(c) / b[2]:get(c), c)
+end
+ops.div = wrapLoop(wrapChan(div))
+
+local function compMult(b, p, c) -- 4, 2
+	b[5]:set( b[1]:get(c)*b[3]:get(c) - b[2]:get(c)*b[4]:get(c), c)
+	b[6]:set( b[1]:get(c)*b[4]:get(c) + b[2]:get(c)*b[3]:get(c), c)
+end
+ops.compMult = wrapLoop(wrapChan(compMult))
+
+local function zero(b, p, c)-- 0,1
+	b[1]:set( 0, c)
+end
+ops.zero = wrapLoop(wrapChan(zero))
+
+local function equaliseGB(b, p) -- 1,1
+	local GB = (b[1]:get(1)+b[1]:get(2))/2
+	b[2]:set( GB, 1)
+	b[2]:set( GB, 2)
+end
+ops.equaliseGB = wrapLoop(equaliseGB)
+
+local function invertR_GB(b, p) -- 1,1
+	local GB = 1-b[1]:get(0)
+	b[2]:set( GB, 1)
+	b[2]:set( GB, 2)
+end
+ops.invertR_GB = wrapLoop(invertR_GB)
+
+--[[
+do
+	local function filter(func, flag)
+		for x = __instance, xmax/2, __tmax do
+			if progress[0]==-1 then break end
+			for y = 0, ymax/2 do
+				local gauss
+				local size = 128 --math.sqrt(xmax^2+ymax^2)
+				gauss = func(math.sqrt(x^2+y^2), get[1](0)*size)
+				gauss = gauss + (flag and func(math.sqrt((xmax-x+1)^2+y^2), get[1](0)*size) or 0)
+				gauss = gauss + (flag and func(math.sqrt(x^2+(ymax-y+1)^2), get[1](0)*size) or 0)
+				gauss = gauss + (flag and func(math.sqrt((xmax-x+1)^2+(ymax-y+1)^2), get[1](0)*size) or 0)
+				gauss = gauss * get[2]() + 1 - get[2]()
+				set3xy[1](gauss, gauss, gauss,x,y)
+				if x~=0 then set3xy[1](gauss, gauss, gauss, xmax-x,y) end
+				if y~=0 then set3xy[1](gauss, gauss, gauss, x, ymax-y) end
+				if x~=0 and y~=0 then set3xy[1](gauss, gauss, gauss, xmax-x, ymax-y) end
+			end
+			progress[__instance+1] = x - __instance
+		end
+		progress[__instance+1] = -1
+	end
+
+	function ops.gauss() return filter(math.func.gauss) end
+	function ops.lorenz() return filter(math.func.lorenz) end
+	function ops.gauss_wrap() return filter(math.func.gauss, true) end
+	function ops.lorenz_wrap() return filter(math.func.lorenz, true) end
+end
 --]]
-end
-
-
 
 --[[
 ops.norm = function()	-- 1,1
