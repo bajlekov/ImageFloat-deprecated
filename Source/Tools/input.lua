bajlekov/ImageFloat-deprecated

commit 979323c635b4b087c238268578e41526c83dc00d

More accurate frame limiter, reset when delay builds up

Source/Tools/input.lua
@@ -148,23 +148,19 @@ return function(SDL)
 		SDLK_z = 122,
 		]]
 
-		-- event functions like on_click etc.
-
 		--timer delay for 60fps
-		while not input.interrupt() do
-			if (startTime+refreshDelay-__sdl.ticks())<0.1 then break end
-			SDL.SDL_Delay(.1)
-		end
-		---[[
-		do
-			local t = __sdl.ticks()-startTime
-			--if t > 33 then print("***",t) end
-		end
-		--]]
-		startTime = __sdl.ticks()
+		if __sdl.ticks()-startTime < 1.25*refreshDelay then 
+			while not input.interrupt() do
+				if __sdl.ticks()-startTime > refreshDelay  then break end
+				SDL.SDL_Delay(1)
+			end 
+			startTime = startTime + refreshDelay
+		else
+			startTime = __sdl.ticks()
+		end		
 
 		--help in detecting unreferenced cdata
-		--__dbg.gc()
+		--collectgarbage("collect")
 	end
 
 	return input

commit 2cb1805d663727cf0bd293fe8b81ae48b3d3b11e

Fix bug introduced in commit 979323c6

Source/Tools/input.lua
@@ -150,14 +150,20 @@ return function(SDL)
 
 		--timer delay for 60fps
 		if __sdl.ticks()-startTime < 1.25*refreshDelay then 
-			while not input.interrupt() do
-				if __sdl.ticks()-startTime > refreshDelay  then break end
-				SDL.SDL_Delay(1)
-			end 
-			startTime = startTime + refreshDelay
+			while true do
+				if __sdl.ticks()-startTime > refreshDelay then
+					startTime = startTime + refreshDelay
+					break
+				end
+				if input.interrupt() then
+					startTime = __sdl.ticks()
+					break
+				end
+				SDL.SDL_Delay(0.1)
+			end
 		else
 			startTime = __sdl.ticks()
-		end		
+		end
 
 		--help in detecting unreferenced cdata
 		--collectgarbage("collect")