bd1678e09e35fc3b931a32b08532fed4f300b134
Galin Bajlekov <g.bajlekov@gmail.com>
2013-07-02 17:59:38 +0200

    Extend compile module with gcc and clang compiling

--- Source/Tools/compile.lua
+++ Source/Tools/compile.lua
@@ -20,12 +20,38 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 local compile = {}
 local ffi = require("ffi")
 
-function compile.ispc(file)
-	os.execute ("ispc --opt=fast-math --pic -o "..file..".o "..file..".ispc") print("compiling... (ispc)")
-	os.execute ("clang -shared -O4 -o "..file..".so "..file..".o") print("linking... (clang)")
+local function writeFile(file, str)
+	local f, err = io.open(file,"w")
+	f:write(str)
+	f:close()
+end
+
+function compile.ispc(file, str)
+	if str~=nil then
+		writeFile(file..".ispc", str)
+	end
+	os.execute ("ispc --pic --math-lib=fast --opt=fast-math --wno-perf --target=avx1 -o "..file..".o "..file..".ispc") print("compiling... (ispc)")
+	os.execute ("clang -shared -o "..file..".so "..file..".o") print("linking... (clang)")
+	return ffi.load("./"..file..".so")
+end
+
+function compile.gcc(file, str)
+	if str~=nil then
+		writeFile(file..".c", str)
+	end
+	os.execute ("gcc -O3 -std=gnu99 -ffast-math -march=native -fPIC -o "..file..".o -c "..file..".c") print("compiling... (gcc)")
+	os.execute ("gcc -shared -o "..file..".so "..file..".o") print("linking... (gcc)")
+	return ffi.load("./"..file..".so")
+end
+
+
+function compile.clang(file, str)
+	if str~=nil then
+		writeFile(file..".c", str)
+	end
+	os.execute ("clang -O3 -std=gnu99 -ffast-math -march=native -fPIC -o "..file..".o -c "..file..".c") print("compiling... (clang)")
+	os.execute ("clang -shared -o "..file..".so "..file..".o") print("linking... (clang)")
 	return ffi.load("./"..file..".so")
 end
 
 return compile
-
-
