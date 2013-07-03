84ebb3786359fe05fe385dbbe59bee3eb9dee31c
Galin Bajlekov <g.bajlekov@gmail.com>
2013-07-03 16:24:06 +0200

    New function for non-AA faster line drawing

--- Source/Draw/draw.lua
+++ Source/Draw/draw.lua
@@ -100,68 +100,98 @@ function boxAdd(x1,y1,x2,y2,r,g,b)
 		end
 	end
 end
+
+local abs = math.abs
+function drawLine_fast(x0, y0, x1, y1, r, g, b)	
+	local steep = abs(y1-y0) > abs(x1-x0)
+	if steep then x0, y0, x1, y1 = y0, x0, y1, x1 end
+	if x0>x1 then x0, x1, y0, y1 = x1, x0, y1, y0 end
+	
+	local dx = x1-x0
+	local dy = abs(y1-y0)
+	local err = dx/2
+	local y = y0
+	local ystep = y0<y1 and 1 or -1
+	
+	for x = x0, x1 do
+		setPixel(steep and y or x, steep and x or y, r, g, b)
+		err = err - dy
+		y = err<0 and y+ystep or y
+		err = err<0 and err + dx or err
+	end
+end
     
-function drawLine(x1, y1, x2, y2, r, g, b)
-	--TODO: handle overlap of connected vertices in endpoint code
-	--TODO: implement connected vertices draw (polyline)
-    --AA line drawing
-    local function ipart(x) return math.floor(x) end
+do
+
+    local floor = math.floor
+    local function ipart(x) return floor(x) end
     local function round(x) return ipart(x+0.5) end
-    local function fpart(x) return x-math.floor(x) end
+    local function fpart(x) return x-floor(x) end
     local function rfpart(x) return 1-fpart(x) end
     local function invpixeladd(x,y,r,g,b) return pixelAdd(y,x,r,g,b) end
-    
-    --avoid zero-length lines
-    if x1==x2 and y1==y2 then return end 
 
-    local dx = x2 - x1
-    local dy = y2 - y1
-    
-    local pixeladd = pixelAdd
-    
-    if math.abs(dx) < math.abs(dy) then
-      x1, y1 = y1, x1
-      x2, y2 = y2, x2
-      dx, dy = dy, dx
-      pixeladd = invpixeladd
-  end
 
-  if x2 < x1 then
-      x1, x2 = x2, x1
-      y1, y2 = y2, y1
-  end
+	function drawLine(x1, y1, x2, y2, r, g, b)
+		--TODO: handle overlap of connected vertices in endpoint code
+		--TODO: implement connected vertices draw (polyline)
+	    --AA line drawing
+	    
+	    --avoid zero-length lines
+	    if x1==x2 and y1==y2 then return end 
+	
+	    local dx = x2 - x1
+	    local dy = y2 - y1
+	    
+	    local pixeladd = pixelAdd
+	    
+	    if math.abs(dx) < math.abs(dy) then
+	      x1, y1 = y1, x1
+	      x2, y2 = y2, x2
+	      dx, dy = dy, dx
+	      pixeladd = invpixeladd
+	  end
+	
+	  if x2 < x1 then
+	      x1, x2 = x2, x1
+	      y1, y2 = y2, y1
+	  end
+	
+	  local gradient = dy / dx
+	
+	    -- handle first endpoint
+	    local xend = round(x1)
+	    local yend = y1 + gradient * (xend - x1)
+	    local xgap = rfpart(x1 + 0.5)
+	    local xpxl1 = xend --this will be used in the main loop
+	    local ypxl1 = ipart(yend)
+	    pixeladd(xpxl1, ypxl1, rfpart(yend)*xgap*r, rfpart(yend)*xgap*g, rfpart(yend)*xgap*b)
+	    pixeladd(xpxl1, ypxl1+1, fpart(yend)*xgap*r, fpart(yend)*xgap*g, fpart(yend)*xgap*b)
+	    
+	    local intery = yend + gradient --first y-intersection for the main loop
+	    
+	    -- handle second endpoint
+	    xend = round (x2)
+	    yend = y2 + gradient * (xend - x2)
+	    xgap = fpart(x2 + 0.5)
+	    local xpxl2 = xend -- this will be used in the main loop
+	    local ypxl2 = ipart (yend)
+	    pixeladd(xpxl2, ypxl2, rfpart(yend)*xgap*r, rfpart(yend)*xgap*g, rfpart(yend)*xgap*b)
+	    pixeladd(xpxl2, ypxl2+1, fpart(yend)*xgap*r, fpart(yend)*xgap*g, fpart(yend)*xgap*b)
+	    
+	    --main loop
+	    for x = xpxl1+1, xpxl2-1 do
+	        pixeladd(x, ipart (intery), rfpart(intery)*r, rfpart(intery)*g, rfpart(intery)*b)
+	        pixeladd(x, ipart (intery)+1, fpart(intery)*r, fpart(intery)*g, fpart(intery)*b)
+	        --pixeladd(x, ipart (intery)+1, -rfpart(intery)*r, -rfpart(intery)*g, -rfpart(intery)*b)
+	        --pixeladd(x, ipart (intery)+2, -fpart(intery)*r, -fpart(intery)*g, -fpart(intery)*b)
+	        intery = intery + gradient
+	    end
+	end
 
-  local gradient = dy / dx
-
-    -- handle first endpoint
-    local xend = round(x1)
-    local yend = y1 + gradient * (xend - x1)
-    local xgap = rfpart(x1 + 0.5)
-    local xpxl1 = xend --this will be used in the main loop
-    local ypxl1 = ipart(yend)
-    pixeladd(xpxl1, ypxl1, rfpart(yend)*xgap*r, rfpart(yend)*xgap*g, rfpart(yend)*xgap*b)
-    pixeladd(xpxl1, ypxl1+1, fpart(yend)*xgap*r, fpart(yend)*xgap*g, fpart(yend)*xgap*b)
-    
-    local intery = yend + gradient --first y-intersection for the main loop
-    
-    -- handle second endpoint
-    xend = round (x2)
-    yend = y2 + gradient * (xend - x2)
-    xgap = fpart(x2 + 0.5)
-    local xpxl2 = xend -- this will be used in the main loop
-    local ypxl2 = ipart (yend)
-    pixeladd(xpxl2, ypxl2, rfpart(yend)*xgap*r, rfpart(yend)*xgap*g, rfpart(yend)*xgap*b)
-    pixeladd(xpxl2, ypxl2+1, fpart(yend)*xgap*r, fpart(yend)*xgap*g, fpart(yend)*xgap*b)
-    
-    --main loop
-    for x = xpxl1+1, xpxl2-1 do
-        pixeladd(x, ipart (intery), rfpart(intery)*r, rfpart(intery)*g, rfpart(intery)*b)
-        pixeladd(x, ipart (intery)+1, fpart(intery)*r, fpart(intery)*g, fpart(intery)*b)
-        --pixeladd(x, ipart (intery)+1, -rfpart(intery)*r, -rfpart(intery)*g, -rfpart(intery)*b)
-        --pixeladd(x, ipart (intery)+2, -fpart(intery)*r, -fpart(intery)*g, -fpart(intery)*b)
-        intery = intery + gradient
-    end
 end
+
+-- use fast line drawing (about 2x faster, another 3x faster due to pixelset instead of pixeladd)
+-- drawLine = drawLine_fast
 
 --colour definitions
 
