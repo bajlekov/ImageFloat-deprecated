098ce24fd1ae1b28e03890714b6df8bb62095844
Galin Bajlekov <g.bajlekov@gmail.com>
2013-07-10 00:38:25 +0200

    Update building process

--- Build/build.sh
+++ Build/build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 cp squishyMain squishy
-squish --minify-level=basic
+./squish --minify-level=basic
 rm squishy
 cp squishyThread squishy
-squish --minify-level=basic
+./squish --minify-level=basic
 rm squishy
 
-luajit -b ImageFloat.lua ImageFloat
-luajit -b Thread.lua Thread
+#luajit -b ImageFloat.lua ImageFloat
+#luajit -b Thread.lua Thread
+cp ImageFloat.lua ImageFloat
+cp Thread.lua Thread
+
+cp ImageFloat ../Distribution/ImageFloat
+cp Thread ../Distribution/Thread
