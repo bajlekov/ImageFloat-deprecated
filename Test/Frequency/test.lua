f95e0dbe526c351c6b21bd5f38fff475567c7a4f
Galin <g.bajlekov@gmail.com>
2014-01-25 21:08:09 +0100

    Test simple pyramid denoising

--- Test/Frequency/test.lua
+++ Test/Frequency/test.lua
@@ -41,6 +41,53 @@ local function imshow(i)
 	end
 end
 
+
+local function f(x)
+	if x<-0.5 then return x+0.25 end
+	if x>0.5 then return x-0.25 end
+	if x<=0 then return -x^2 end
+	if x>0 then return x^2 end
+end
+
+local function filter(i, t)
+	-- remove coefficients close to 0
+	
+	for x = 0, i.x-1 do
+		for y = 0, i.y-1 do
+			for z = 0, i.z-1 do
+				local v = i:i(x, y, z)
+				i:a(x, y, z, f(v/t)*t)
+			end
+		end
+	end
+	
+end
+
+for x = 0, i.x-1 do
+	for y = 0, i.y-1 do
+		i:a(x,y,0, i:i(x,y,0)+math.random()*0.1)
+		i:a(x,y,1, i:i(x,y,1)+math.random()*0.1)
+		i:a(x,y,2, i:i(x,y,2)+math.random()*0.1)
+	end
+end
+
+sdl.screen.set(i.x, i.y)
+imshow(i)
+
+local L, G = pyr.construct(i)
+filter(L[0], 0.07)
+filter(L[1], 0.03)
+filter(L[2], 0.01)
+local i = pyr.collapse(L)
+
+imshow(i)
+imshow(original)
+imshow(i)
+imshow(original)
+imshow(i)
+imshow(original)
+
+--[[
 local n = 6
 local L, G = pyr.construct(i,n)
 --L[n] = L[n]:new()+1/2
@@ -59,9 +106,10 @@ L[n-1] = L[n-1]/3
 L[0] = L[0]*3
 L[1] = L[1]*3/2
 L[2] = L[2]*3/4
+filter(L[0], 0.005)
 local i = pyr.collapse(L)
 
 sdl.screen.set(i.x, i.y)
 imshow(i)
-
+--]]
 
