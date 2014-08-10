100a887ccac0f7b429ed641084d6700d7d16d4c7
Galin <g.bajlekov@gmail.com>
2014-08-10 11:28:13 +0200

    Use SDL2, make runnable from both current and parent directories

--- Source/Threads/threadFunc.lua
+++ Source/Threads/threadFunc.lua
@@ -17,6 +17,8 @@
 
 print("Thread initialisation...")
 
+package.path = package.path .. ';Source/?.lua'
+
 -- disable implicit globals
 do
 	function global(k, v) -- assign new global
@@ -52,7 +54,7 @@ __global.setup = require("Setup.IFsetup")
 __global.libPath = __global.setup.libPath or "./Libraries/"..ffi.os.."_"..ffi.arch.."/"
 
 -- replace the complete sdl lib with just the mutex functions and possibly tick/wait!
-local sdl = require("Include.sdl")
+local sdl = require("Include.sdl2")
 global("__sdl", sdl)
 
 if __global.setup.optCompile.ispc then
