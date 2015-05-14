a31d757e3c2cdc4f56f0cd9e8de3113983a42c71
Galin Bajlekov <g.bajlekov@gmail.com>
2015-05-15 00:46:18 +0200

    Updated nlmeans algorithm

--- Test/Raw/Denoise/nlmeans.lua
+++ Test/Raw/Denoise/nlmeans.lua
@@ -19,81 +19,105 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- implemented from Secrets of image denoising cuisine
 
 local ffi = require("ffi")
-local unroll = require("Tools.unroll")
+local lut = require("Tools.lut")
+lut.linear = true
 
 local l = 2
-local k = (l*2+1)^2-1
+local k = (l*2+1)^2
+
+local P = ffi.new("double[?]", k)
+local Q = ffi.new("double[?]", k)
+local N = ffi.new("double[?]", k)
+local O = ffi.new("double[?]", k)
 
 local function d2(a, b)
 	local sum = 0
-	for i = 0, k do
+	for i = 0, k-1 do
 		sum = sum + (a[i]-b[i])^2
 	end
-	return sum/(k+1)
+	return sum/k
 end
 
-local P = ffi.new("double[?]", k+1)
-local Q = ffi.new("double[?]", k+1)
-
-local unroll = unroll.construct(-l, l, -l, l)
-
-local function getPfun(k, l, i, x, y, c)
-	P[c] = i:i(x+k,y+l)
-end
-local function getQfun(k, l, i, x, y, c)
-	Q[c] = i:i(x+k,y+l)
-end
-
-local function getP(i, x, y)
+-- unrolling does not help!
+local function getP(P, i, x, y)
 	local c = 0
 	for k = -l, l do
-		for l = -l, l do
-			P[c] = i:i(x+k,y+l)
+		for m = -l, l do
+			--P[(k+l)*(2*l+1)+(m+l)] = i:i(x+k,y+m)
+			P[c] = i:i(x+k,y+m)
 			c = c+1
 		end
 	end
 end
 
-local function getQ(i, x, y)
-	local c = 0
-	for k = -l, l do
-		for l = -l, l do
-			Q[c] = i:i(x+k,y+l)
-			c = c+1
-		end
-	end
-end
-
+local exp = math.exp
 -- implement larger patches for raw denoising
-
-local function nlmeans(i, sigma)
+local function nlmeans(i, sigma, ii)	-- use ii as a predictor if available
 	local o = i:new()
-	sigma = sigma or 0.05
-	local l = 8 -- *2+1
-	local sigma = sigma^2
+	sigma = 1/(sigma or 0.05)^2
+	local ll = l
+	local kk = 1/k
+	local l = 16 -- *2+1
+	local mid = (k-1)/2
 	
-	for x = l+2, i.x-l-3 do
-		for y = l+2, i.y-l-3 do
+	local explut = lut.create(function(x) return exp(-x*sigma) end, 0, 2, 1024, 0) -- TODO: possibly tune, too tricky at present
+	
+	for x = l+ll, i.x-l-ll-1 do -- optional step of patch size for speedup
+		for y = l+ll, i.y-l-ll-1 do
 			
-			getP(i, x, y)
+			getP(P, i, x, y)
 			
-			local Phat = 0
+			--local Phat = 0
 			local Chat = 0
+			
+			for i = 0, k-1 do
+				N[i] = 0
+			end
 			
 			for px = -l, l, 2 do
 				for py = -l, l, 2 do
-					getQ(i, x+px, y+py)
 					
-					local t = math.exp(-d2(P, Q)/sigma)
-					Phat = Phat + Q[k/2]*t
+					getP(Q, i, x+px, y+py)
+					if ii then
+						getP(O, ii, x+px, y+py)
+					end
+					
+					local t
+					if ii then
+						t = exp(-d2(P, O)*sigma)
+						--local t = explut(d2(P, O))
+					else
+						t = exp(-d2(P, Q)*sigma)
+						--local t = explut(d2(P, Q))
+					end
+					
+					--Phat = Phat + Q[mid]*t
 					Chat = Chat + t
+					
+					for i = 0, k-1 do
+						N[i] = N[i] + Q[i]*t
+					end
+					
 				end
 			end
 			
-			o:a(x, y, Phat/Chat)
+			Chat = 1/Chat
+			
+			do
+				local c = 0
+				for k = -ll, ll do
+					for m = -ll, ll do
+						o:a(x+k,y+m,0, o:i(x+k,y+m,0) + N[c]*Chat*kk)
+						--o:a(x+k,y+m,0, o:i(x+k,y+m,0) + N[c]*Chat)
+						c = c+1
+					end
+				end
+			end
+			
+			--o:a(x,y,0, Phat*Chat)
 			
 		end
-		print(x)
+		--io.write(x)
 	end
 	
 	return o
